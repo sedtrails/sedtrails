3bf81a31da0db57b11913632629921cb16a95747
Hassan SHafiei <h.shafiei@tudelft.nl>
2025-08-27 13:47:27 +0200

    delete some more files to see

--- src/sedtrails/transport_converter/plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-from .physics.plugin import BasePhysicsPlugin
-from .format.plugin import BaseFormatPlugin
-
-__all__ = [
-            "BasePhysicsPlugin",
-            "BaseFormatPlugin"
-           ]
--- tests/converters/test_format_converter.py
+++
@@ -1,7 +0,0 @@
-"""Test for the Format Converter"""
-
-def test_blank():
-    """
-    This is a placeholder test function 
-    """
-    assert 1 + 1 == 2
