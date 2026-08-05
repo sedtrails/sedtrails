fix: broadcast MacDonald settling velocity

src/sedtrails/transport_converter/plugins/physics/macdonald.py
@@ -198,8 +198,24 @@ class PhysicsPlugin(BasePhysicsPlugin):  # all classes should be called the Phys
 
             # suspended load transport fraction, equation 31 MacDonald et al. (2006)
             qs_qt = np.full_like(selected_shear_velocity, np.nan, dtype=float)
-            mask = (selected_shear_velocity > 0) & (settling_velocity > 0) & np.isfinite(selected_shear_velocity) & np.isfinite(settling_velocity)
-            qs_qt[mask] = (0.5 * np.tanh(1.3 * np.log(selected_shear_velocity[mask] / settling_velocity[mask]) - 0.3) + 0.5)            
+            settling_velocity_field = np.broadcast_to(
+                np.asarray(settling_velocity, dtype=float),
+                selected_shear_velocity.shape,
+            )
+            mask = (
+                (selected_shear_velocity > 0)
+                & (settling_velocity_field > 0)
+                & np.isfinite(selected_shear_velocity)
+                & np.isfinite(settling_velocity_field)
+            )
+            qs_qt[mask] = 0.5 * np.tanh(
+                1.3
+                * np.log(
+                    selected_shear_velocity[mask]
+                    / settling_velocity_field[mask]
+                )
+                - 0.3
+            ) + 0.5
 
         # suspended load height (MacDonald et al., 2006, equation 27)
         z_s = PhysicsPlugin.calculate_macdonald_susp_load_height(rouse_number, water_depth)

tests/transport_converter/plugins/test_macdonald_plugin.py
@@ -6,7 +6,7 @@ from types import SimpleNamespace
 import numpy as np
 import pytest
 
-from sedtrails.transport_converter.physics_converter import PhysicsConfig
+from sedtrails.transport_converter.physics_converter import PhysicsConfig, PhysicsConverter
 from sedtrails.transport_converter.plugins.physics.macdonald import PhysicsPlugin
 
 
@@ -261,3 +261,21 @@ def test_add_physics_required_field_values_unaffected_by_export_diagnostics_flag
                 np.testing.assert_array_equal(value_off[key], value_on[key], err_msg=f'{field}[{key}] differs')
         else:
             np.testing.assert_array_equal(value_off, value_on, err_msg=f'{field} differs')
+
+
+def test_soulsby_vanrijn_transport_accepts_scalar_settling_velocity():
+    """Broadcast the converter's scalar settling velocity over field arrays."""
+    config = _macdonald_config(use_transport_fields='SoulsbyvanRijn1997')
+    converter = PhysicsConverter(config.as_dict(), {'macdonald': {}})
+    settling_velocity = converter.grain_properties['settling_velocity']
+    sedtrails_data = _MacdonaldSedtrailsDataStub()
+
+    assert np.asarray(settling_velocity).ndim == 0
+
+    PhysicsPlugin(config, tracer_methods={}).add_physics(
+        sedtrails_data,
+        converter.grain_properties,
+        transport_probability_method='no_probability',
+    )
+
+    assert sedtrails_data.centroid_particle_velocity['magnitude'].shape == sedtrails_data.water_depth.shape